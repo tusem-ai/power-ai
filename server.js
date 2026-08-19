2f80edd3e8417d76d1115a04ce69428deac05b64
tusem <tusemnalsa5783@gmail.com>
2026-08-20 02:36:20 +0300

    site arayuzu eklendi

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
 const PORT = process.env.PORT || 8001;
 
 app.use(express.json());
 
+// Public klasöründeki HTML, CSS ve JS dosyalarını dışarı sunar
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Ana sayfaya girildiğinde index.html dosyasını yükler
 app.get('/', (req, res) => {
-  res.send('Power AI Chatbot Sunucusu Çalışıyor!');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(PORT, '0.0.0.0', () => {
