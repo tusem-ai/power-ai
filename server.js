67c5426b213fbf96b1e84a5a6ccf0bc98b9445a2
tusem <tusemnalsa5783@gmail.com>
2026-08-20 03:00:47 +0300

    index.html yol hatasi duzeltildi

--- server.js
+++ server.js
@@ -6,12 +6,17 @@ const PORT = process.env.PORT || 10000;
 
 app.use(express.json());
 
-// Ana klasördeki statik dosyaları (index.html, style.css vb.) dışarı sunar
+// 1. Hem public klasörünü hem ana dizini dışarı sun
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(__dirname));
 
-// Ana sayfaya girildiğinde direkt ana klasördeki index.html dosyasını gönderir
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+// 2. index.html ister public içinde olsun ister ana dizinde, bulup açar
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'), (err) => {
+    if (err) {
+      res.sendFile(path.join(__dirname, 'index.html'));
+    }
+  });
 });
 
 app.listen(PORT, '0.0.0.0', () => {
